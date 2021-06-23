a76f43bd7f3e33e3aa3b6b7adcb6651754fb205a
Marcel Hoyer <mhoyer@pixelplastic.de>
2021-06-23 20:36:02 +0200

    update all dependencies to latest version

--- lib/createActionDispatchers.ts
+++ lib/createActionDispatchers.ts
@@ -8,7 +8,7 @@ export type Store = {
  * Converts a tree of ducks into a tree of self-dispatching actions
  * by wrapping each duck with store.dispatch().
  */
-export function createActionDispatchers<TDuckTree>(ducks: TDuckTree, store: Store): TDuckTree {
+export function createActionDispatchers<TDuckTree extends object>(ducks: TDuckTree, store: Store): TDuckTree {
     const createDispatchedActionHandler = (origActionHandler) => {
         return function() {
             const action = origActionHandler.apply(this, arguments);
@@ -35,7 +35,7 @@ export function createActionDispatchers<TDuckTree>(ducks: TDuckTree, store: Stor
 /**
  * @deprecated renamed to `createActionDispatchers()`.
  */
-export function createDispatchedActions<TDuckTree>(ducks: TDuckTree, store: Store): TDuckTree {
+export function createDispatchedActions<TDuckTree extends object>(ducks: TDuckTree, store: Store): TDuckTree {
     console.info(`Function \`createDispatchedActions()\` is marked OBSOLETE. Use \`createActionDispatchers()\` instead.`);
     return createActionDispatchers(ducks, store);
 }
